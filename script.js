ecda3c5d69a7bb6008c00c9b32e818ce1eba0bcc
dilip.kumar <dilip.kumar@zohocorp.com>
2022-06-05 17:54:29 +0530

    Added card preview with navigation

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var FoodOrder = function() {
 	this.cardholderdom = document.getElementById('cardholder');
 	this.taglistdom = document.getElementById('taglist');
 	this.darkmodecheckbox = document.getElementById('darkmode');
+	this.modalwindowdom = '';
+	this.currentpreviewid = '';
 	this.isdarkchoosed = false;
 }
 
@@ -27,22 +29,33 @@ FoodOrder.prototype = {
 		document.onclick = (ev) => {
 
 			var elem = ev.target;
+			var purpose = elem.getAttribute('purpose');
 
-			if(elem.getAttribute('purpose') === 'tag') {
-				
-				var tagname = elem.innerText;
-
-				elem.classList.contains('active') ? this.selectedtag.splice( this.selectedtag.indexOf(tagname), 1) : this.selectedtag.push(tagname);
-
-				elem.classList.toggle('active', !elem.classList.contains('active'))
-				
-				this.handleTagFilter(this.selectedtag, this.cardholderdom)
-				
+			if( purpose === 'tag') {
+				this.handleTagSelection(elem)
 			}
+			else if ( purpose == 'card' || elem.closest('[purpose="card"]') && elem.closest('[purpose="card"]').getAttribute('purpose') == 'card' ) {
+				var cardelem = elem.closest('[purpose="card"]');
+				
+				this.handleCardPreview(cardelem.id)
+			}
+			else if( purpose ==  'closemodal' ) {
+				this.modalwindowdom.remove();
+				this.currentpreviewid='';
+			}
+			else if( purpose == 'leftnav' ) {
+				this.handlePreviewAction('left', this.currentpreviewid )
+			}
+			else if( purpose == 'rightnav' ) {
+				this.handlePreviewAction('right', this.currentpreviewid )
+			}
+
+
 
 			if(elem.id === 'darkmode') {
 				this.handledarkmode(ev);
 			}
+
 		}
 
 		document.onkeyup = (ev) => {
@@ -61,14 +74,14 @@ FoodOrder.prototype = {
 	},
 
 	getCard: function(item) {
-		return `<article class="card">
+		return `<article class="card" purpose="card" id="${item.id}">
 					<div class="card-img flexM fshrink"><img src="${this.imagepath+''+item.image}"></div>
-					<div class="flexG mT10 pLR10">
+					<div class="flexG mT10 pLR10 card-content">
 						<div class="flexC">
-							<div class="card-title fontB flexG font15">${item.name}</div>
+							<div class="card-title fontB flexG">${item.name}</div>
 							<div class="card-rating">${item.rating}</div>
 						</div>
-						<div class="clr-S mT10">${item.location}</div>
+						<div class="clr-S mT10 card-location">${item.location}</div>
 					</div>
 					<div class="fshrink clr-S flexC card-tags">${item.tags.join('<span class="clr-T mLR4">|</span>')}</div>
 				</article>`
@@ -130,6 +143,16 @@ FoodOrder.prototype = {
 		this.taglistdom.innerHTML = this.getTags( this.getData() );
 	},
 
+	handleTagSelection: function(elem) {
+		var tagname = elem.innerText;
+
+		elem.classList.contains('active') ? this.selectedtag.splice( this.selectedtag.indexOf(tagname), 1) : this.selectedtag.push(tagname);
+
+		elem.classList.toggle('active', !elem.classList.contains('active'))
+		
+		this.handleTagFilter(this.selectedtag, this.cardholderdom)
+	},
+
 	handleTagFilter: function(selectedtag, containerdom) {
 		var datalist = this.getData();
 		var tagfilteredlist = [];
@@ -171,6 +194,53 @@ FoodOrder.prototype = {
 
 	getLocalStorageData: function(key) {
 		return localStorage.getItem(key);
+	},
+
+	getLoaderHtml: function() {
+		return `<span class="loader"></span></div>`;
+	},
+
+	handleCardPreview: function(id) {
+		
+		this.currentpreviewid = id;
+
+		var modalhtml = `<div class="modal flexM" id="modalwindow">
+							<span class="closeIcon flexM" purpose="closemodal">&#x2715</span>
+							<span class="arrowIcon leftArrow flexM" purpose="leftnav"></span>
+							<span class="arrowIcon rightArrow flexM" purpose="rightnav"></span>
+							<div class="modal-main" id="modalmain"></div>
+						</div>`;
+
+		document.body.innerHTML += modalhtml;
+
+		this.modalwindowdom = document.getElementById('modalwindow')
+
+		this.handleCardPreviewUpdate(id);
+	},
+
+	handleCardPreviewUpdate: function(id) {
+
+		var curcard = this.getData().filter((item) => item.id == id )[0];
+
+		var carddetailhtml = this.getCard(curcard)
+
+		this.modalwindowdom.querySelector('#modalmain').innerHTML = carddetailhtml;
+	},
+
+	handlePreviewAction: function(dir, id) {
+		
+		if( ( dir == 'left' && id == 1 ) || ( dir == 'right' && id == this.getData().length ) ) {
+			return;
+		}
+		if( dir == 'left' ) {
+			--this.currentpreviewid;
+		} else if(dir == 'right') {
+			++this.currentpreviewid;
+		}
+
+		this.handleCardPreviewUpdate( this.currentpreviewid )
 	}
+
+
 
 }
